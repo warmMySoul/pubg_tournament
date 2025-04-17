664d1e8342bb96b3c9eccfc2b006665c9edf5f5b
Danil <61860063+warmMySoul@users.noreply.github.com>
2025-04-17 09:42:43 +0300

    Update admin_action_log.py fix datetime.now
    Исправил автоматическую подстановку даты при создании лога

--- models/admin_action_log.py
+++ models/admin_action_log.py
@@ -6,6 +6,6 @@ class AdminActionLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
     action = db.Column(db.String(255), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.now(ZoneInfo("Europe/Moscow")))
+    timestamp = db.Column(db.DateTime, default=lambda: datetime.now(ZoneInfo("Europe/Moscow")))
 
     user = db.relationship('User', backref='action_logs')
